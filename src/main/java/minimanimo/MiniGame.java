6e6769776d6c400fd327310a578814cb15956ec5
gy <gracekim6531@naver.com>
2025-12-05 02:31:37 +0900

    [CHORE] Setup MiniGame Interface for game architecture

--- src/main/java/minimanimo/MiniGame.java
+++ src/main/java/minimanimo/MiniGame.java
@@ -1,10 +1,18 @@
 package minimanimo;
 
+import java.util.Scanner;
 
 public interface MiniGame {
+
+    
+    //Returns the display name of the game.
     String getGameName();
 
-    int startGame(User user, java.util.Scanner scanner);
 
+    //Starts the game for a given user and returns the score.
+    int startGame(User user, Scanner scanner);
+
+
+    //Indicates whether a lower score means a better performance.
     boolean isLowerScoreBetter();
 }
